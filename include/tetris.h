68ccdf52c277a492de8fa6e5e138e767944b71c2
João Neri <joaogabriel.neri@hotmail.com>
2025-05-06 18:32:43 -0300

    Update tetris.h

--- include/tetris.h
+++ include/tetris.h
@@ -1,1 +1,11 @@
+#ifndef TETRIS_H
+#define TETRIS_H
 
+#include "game.h"
+#include <cli-lib.h>
+
+void draw_game(const GameState *game);
+void process_input(GameState *game, int key);
+void run_game();
+
+#endif
