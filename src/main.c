115001bb631367d47d8eef2474aaf535c84600d1
João Neri <joaogabriel.neri@hotmail.com>
2025-05-06 18:33:25 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,27 +1,6 @@
-#include <string.h>
-#include "screen.h"
-#include "keyboard.h"
-#include "timer.h"
+#include "tetris.h"
 
 int main() {
-    static int ch = 0;
-    screenInit(1);
-    keyboardInit();
-    timerInit(50);
-    screenUpdate();
-
-    while (ch != 10) {
-        if (keyhit()) {
-            ch = readch();
-            screenUpdate();
-        }
-    if (timerTimeOver() == 1) {
-        screenUpdate();
-        }
-    }
-
-    keyboardDestroy();
-    screenDestroy();
-    timerDestroy();
-return 0;
+    run_game();
+    return 0;
 }
