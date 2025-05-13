2bc7e4234d3cf85f6b77b40b49dd18059a5f7930
Pedro Pessoa <pedropessoa0123@gmail.com>
2025-05-13 17:51:20 -0300

    Update tetris.h

--- include/tetris.h
+++ include/tetris.h
@@ -1,11 +1,27 @@
-#ifndef TETRIS_H
-#define TETRIS_H
+#ifndef __TETRIS_H__
+#define __TETRIS_H__
 
-#include "game.h"
-#include <cli-lib.h>
+#include "types.h"
+#include "keyboard.h"
 
-void draw_game(const GameState *game);
-void process_input(GameState *game, int key);
-void run_game();
+#define BOARD_START_X 10
+#define BOARD_START_Y 2
+#define NEXT_BLOCK_X 25
+#define NEXT_BLOCK_Y 2
+
+#define KEY_LEFT 'a'
+#define KEY_RIGHT 'd'
+#define KEY_DOWN 's'
+#define KEY_ROTATE 'w'
+#define KEY_PAUSE 'p'
+#define KEY_QUIT 'q'
+
+void initDisplay();
+void drawBlock(int x, int y, int shape[4][4], int color);
+void drawBoard(GameState *game);
+void drawNextBlock(GameState *game);
+void drawScore(GameState *game);
+void drawGameOver();
+void handleInput(GameState *game);
 
 #endif
